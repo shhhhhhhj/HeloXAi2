22f466a70e1f9fdf7ce73ac21621691570e4c39e
shhhhhhhj <shh120@protonmail.com>
2026-05-29 16:08:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://heloxai.xyz"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
